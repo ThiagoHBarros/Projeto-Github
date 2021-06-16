8a475de555d439001e3bef8cbf006fabaf55b15c
ThiagoHBarros <thiagohbarros1@hotmail.com>
2021-06-15 21:30:00 -0300

    commit busca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BuscaComponent } from './busca/busca.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BuscaComponent
   ],
   imports: [
     BrowserModule,
